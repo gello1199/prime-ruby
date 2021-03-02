64099aa332307d9b472130de6a6449e4a13c56b3
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-03-02 13:24:28 -0500

    commit tree c59e597335e6c00ea920a8512107cb9fe9a13b2e

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require 'pry'
+
+def prime?(integer)
+    if integer > 1
+        range = (2...integer -1).to_a
+        range.each {|num| return false if integer % num == 0}
+        true
+    else
+        false
+    end
+end
